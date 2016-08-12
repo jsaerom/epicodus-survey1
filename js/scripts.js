fac0c06d80786b494cda9f62a2917548b1f671dd
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2016-08-11 23:24:27 -0700

    Adds jQuery and JavaScript in the scripts.js file so that when user clicks on submit button it will take them to the intro.html page

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
+  $("#nameForm").submit(function(){
+    window.open("intro.html");
+  });
+  $("#epicodusForm").submit(function(){
+    
+  });
 
 
 
 
-
-
+// This is end tag for ready function
 });
