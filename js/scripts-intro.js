6fbf4d753ec3f44d4480a3d437b5c6ce028b4b12
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2016-08-12 10:00:59 -0700

    Adds branching to scripts.js so that when users submit the form it will take them to the proper results page

---
+++ js/scripts-intro.js
@@ -0,0 +1,4 @@
+// $(document).ready(function(){
+//   $("#introPage").load
+//
+// });
--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,34 @@
 $(document).ready(function(){
+// Do not mess with anything above this
   $("#nameForm").submit(function(){
+    var name = $("input#name").val();
+
     window.open("intro.html");
+
   });
+
   $("#epicodusForm").submit(function(){
-    
+    var experience = $("#experience").val();
+    var passion = $("input:radio[name=enjoy]:checked").val();
+    var frontBack = $("input:radio[name=devType]:checked").val();
+    var size = $("input:radio[name=companySize]:checked").val();
+    var project = $("input:radio[name=projectType]:checked").val();
+
+    if (passion == "No"){
+      window.open("dont.html");
+    } else if (passion == "Yes" && frontBack == "Front"){
+      window.open("css.html");
+    } else if (passion == "Yes" && frontBack == "Back" && size == "Startups"){
+      window.open("ruby.html");
+    } else {
+      window.open("java.html");
+    }
   });
 
+// This is the end tag for ready function
+// });
 
 
 
-// This is end tag for ready function
+
 });
